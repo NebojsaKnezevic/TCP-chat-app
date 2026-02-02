1c03b86b91b4bed46663da0c3ee898cdbd3db885
Nebojsa Knezevic [EXT] <nebojsa.knezevic@ch.abb.com>
2026-02-02 20:49:05 +0100

    Init commit

---
+++ src/server/server.ts
@@ -0,0 +1,21 @@
+import { createServer, Socket } from "net";
+
+const server = createServer();
+
+server.on("connection", (socket: Socket) => {
+  socket.setEncoding("utf-8");
+  socket.on("data", (data) => {
+    console.log("Accepted:", data);
+  });
+  socket.on("error", () => {
+    console.log("Connection ended!");
+  });
+  socket.write("Hello from server");
+  socket.on("drain", () => {
+    socket.write("Hello from server");
+  });
+});
+
+server.listen(3099, "127.0.0.1", () => {
+  console.log("Server running! ", server.address());
+});
